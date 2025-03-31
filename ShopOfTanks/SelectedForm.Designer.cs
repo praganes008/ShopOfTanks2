669b8c7f9a14149bf1ed6edf45a3d1c508edf0b1
praganes008 <akim.ivan777@gmail.com>
2025-03-31 18:31:36 +0400

    Избранное. Вывод параметров.

--- ShopOfTanks/SelectedForm.Designer.cs
+++ ShopOfTanks/SelectedForm.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(898, 599);
+            this.ClientSize = new System.Drawing.Size(1198, 599);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SelectedForm";
--- ShopOfTanks/SelectedForm.cs
+++ ShopOfTanks/SelectedForm.cs
@@ -27,22 +27,90 @@ namespace ShopOfTanks
             {
                 Product product = select_product.Key;
 
+                #region 1 столбец - Картинка
                 PictureBox pic = new PictureBox();
-
                 pic.Location = new Point(x, y);
                 pic.Size = new Size(171, 166);
                 pic.SizeMode = PictureBoxSizeMode.Zoom;
                 pic.Image = product.picture.Image;
                 Controls.Add(pic);
+                #endregion
 
-                Label lbl = new Label();
-                lbl.Location = new Point(x+300, y);
-                lbl.Size = new Size(200, 20);
-                lbl.Text = "Количество:" + select_product.Value.ToString();
-                Controls.Add(lbl);
+                #region 2 столбец - Характеристики
+                Label lbl_name = new Label();
+                lbl_name.Location = new Point(x + 250, y);
+                lbl_name.Size = new Size(200, 20);
+                lbl_name.Text = "Товар: " + select_product.Key.name.ToString();
+                Controls.Add(lbl_name);
+
+                Label lbl_country = new Label();
+                lbl_country.Location = new Point(x + 250, y+20);
+                lbl_country.Size = new Size(300, 20);
+                lbl_country.Text = "Страна производства: " + select_product.Key.country.ToString();
+                Controls.Add(lbl_country);
+
+                Label lbl_massa = new Label();
+                lbl_massa.Location = new Point(x + 250, y + 40);
+                lbl_massa.Size = new Size(300, 20);
+                lbl_massa.Text = "Средняя масса машины(тонны): " + select_product.Key.massa.ToString();
+                Controls.Add(lbl_massa);
+
+                Label lbl_type = new Label();
+                lbl_type.Location = new Point(x + 250, y + 60);
+                lbl_type.Size = new Size(300, 20);
+                lbl_type.Text = "Тип танка: " + select_product.Key.type.ToString();
+                Controls.Add(lbl_type);
+
+                Button btn_opis = new Button();
+                btn_opis.Location = new Point(x + 250, y+90);
+                btn_opis.Size = new Size(150, 35);
+                btn_opis.Text = "К описанию";
+                btn_opis.Tag = select_product.Key.name;
+                btn_opis.Click += new EventHandler(open_product);
+                Controls.Add(btn_opis);
+                #endregion
+
+                #region 3 столбец - Количество + Цена
+                Label lbl_count = new Label();
+                lbl_count.Location = new Point(x+650, y);
+                lbl_count.Size = new Size(300, 20);
+                lbl_count.Text = "Количество:";
+                Controls.Add(lbl_count);
+
+                NumericUpDown kol = new NumericUpDown();
+                kol.Location = new Point(x + 650, y + 25);
+                kol.Size = new Size(150, 20);
+                kol.Value = select_product.Value;
+                Controls.Add(kol);
+
+                Label lbl_price = new Label();
+                lbl_price.Location = new Point(x + 650, y+55);
+                lbl_price.Size = new Size(300, 20);
+                lbl_price.Text = "Цена: " + select_product.Key.price.ToString();
+                Controls.Add(lbl_price);
+
+                Label lbl_stoim = new Label();
+                lbl_stoim.Location = new Point(x + 650, y + 75);
+                lbl_stoim.Size = new Size(300, 20);
+                lbl_stoim.Text = "Стоимость: ";
+                Controls.Add(lbl_stoim);
+                #endregion
 
                 y += 220;
             }
+        }
+        private void open_product(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            foreach (KeyValuePair<Product, int> select_product in selectProduct)
+            {
+                if (btn.Tag.ToString() == select_product.Key.name)
+                {
+                    ProductForm frm = new ProductForm(select_product.Key);
+                    frm.Show();
+                }
+            }
+
         }
     }
 }
