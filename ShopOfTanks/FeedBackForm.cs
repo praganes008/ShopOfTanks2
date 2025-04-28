99a64cda77422da3dfd5675ebeca9050e62ef01e
praganes008 <akim.ivan777@gmail.com>
2025-04-28 18:22:20 +0400

    Форма обратной связи готова! Ну, типа мы сами себе отсылаем тех поддержку, и тд.

--- ShopOfTanks/FeedBackForm.cs
+++ ShopOfTanks/FeedBackForm.cs
@@ -23,15 +23,36 @@ namespace ShopOfTanks
         {
             //адрес исходника(от кого)
             MailAddress fromAdress = new MailAddress("serzh.povorov.95@mail.ru");
+            if (TopicCB.Text == "")
+            {
+                MessageBox.Show("Поле ТЕМА обязательно к заполнению!");
+            }
+            else
+            {
 
-            //адрес кому
-            MailAddress toAdress = new MailAddress("serzh.povorov.95@mail.ru");
+                //адрес кому
+                MailAddress toAdress = new MailAddress("serzh.povorov.95@mail.ru");
+                //формирование письма
+                using (MailMessage mailMessage = new MailMessage(fromAdress, toAdress))
+                using (SmtpClient smtpClient = new SmtpClient())                                                             //смтп - отправка сообщенийй
+                {
+                    //тема письма
+                    mailMessage.Subject = TopicCB.Text;                                                                         //присвоение темы письму из комбобокса
+                                                                                                                                //тело(текст) письма                                                                                                            //текст письма
+                    mailMessage.Body = MessageTB.Text;
+                    mailMessage.IsBodyHtml = true;                                                                                  //при формировании сообщ. текст письма будет представлен в html
 
-            //тема письма
-
-
-            //текст письма
-
+                    smtpClient.Host = "smtp.mail.ru";         //берётся из самого "ящика"
+                    smtpClient.Port = 587;                    //берётся из самого "ящика" 
+                    smtpClient.EnableSsl = true;                                                                             //метод шифрования, конфидециальность...
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;                                                      //метод отправки
+                    smtpClient.UseDefaultCredentials = false;                                                                        //тоже безопасность
+                    smtpClient.Credentials = new NetworkCredential(fromAdress.Address, "erfHctFyUSzczxkna0cw");                                      //откуда будет отправляться, а второй параметр это пароль, который генерируется в самом почтовом ящике в разделе"безопасность"
+                    smtpClient.Send(mailMessage);                                                                               //отправка письма 
+                }
+                MessageBox.Show("Сообщение успешно отправлено!");
+                Close();
+            }
 
         }
     }
