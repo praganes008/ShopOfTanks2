c5707a4dd633df667315066fb8b455ae88745804
praganes008 <akim.ivan777@gmail.com>
2025-12-08 19:30:19 +0400

    Начал и врод закончил наполнять форму добавления запчастей функционалом TB CB и Btn

--- ShopOfTanks/AddToolForm.Designer.cs
+++ ShopOfTanks/AddToolForm.Designer.cs
@@ -29,98 +29,111 @@
         private void InitializeComponent()
         {
             this.InfoPanel = new System.Windows.Forms.Panel();
-            this.ButtonPanel = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.AddToolBtn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PriceTB = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.LoadPicBtn = new System.Windows.Forms.Button();
+            this.NameTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OpisanieTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TankComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PicBox = new System.Windows.Forms.PictureBox();
+            this.ButtonPanel = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.InfoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
             this.ButtonPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // InfoPanel
             // 
             this.InfoPanel.BackColor = System.Drawing.Color.MediumBlue;
+            this.InfoPanel.Controls.Add(this.AddToolBtn);
+            this.InfoPanel.Controls.Add(this.label7);
+            this.InfoPanel.Controls.Add(this.PriceTB);
             this.InfoPanel.Controls.Add(this.label6);
-            this.InfoPanel.Controls.Add(this.button1);
-            this.InfoPanel.Controls.Add(this.textBox2);
+            this.InfoPanel.Controls.Add(this.LoadPicBtn);
+            this.InfoPanel.Controls.Add(this.NameTB);
             this.InfoPanel.Controls.Add(this.label5);
             this.InfoPanel.Controls.Add(this.label3);
-            this.InfoPanel.Controls.Add(this.textBox1);
+            this.InfoPanel.Controls.Add(this.OpisanieTB);
             this.InfoPanel.Controls.Add(this.label2);
-            this.InfoPanel.Controls.Add(this.comboBox1);
+            this.InfoPanel.Controls.Add(this.TankComboBox);
             this.InfoPanel.Controls.Add(this.label1);
-            this.InfoPanel.Controls.Add(this.pictureBox1);
+            this.InfoPanel.Controls.Add(this.PicBox);
             this.InfoPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.InfoPanel.Location = new System.Drawing.Point(0, 0);
             this.InfoPanel.Name = "InfoPanel";
             this.InfoPanel.Size = new System.Drawing.Size(1181, 521);
             this.InfoPanel.TabIndex = 0;
             // 
-            // ButtonPanel
+            // AddToolBtn
             // 
-            this.ButtonPanel.BackColor = System.Drawing.Color.Lime;
-            this.ButtonPanel.Controls.Add(this.button2);
-            this.ButtonPanel.Controls.Add(this.label4);
-            this.ButtonPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.ButtonPanel.Location = new System.Drawing.Point(0, 440);
-            this.ButtonPanel.Name = "ButtonPanel";
-            this.ButtonPanel.Size = new System.Drawing.Size(1181, 137);
-            this.ButtonPanel.TabIndex = 1;
+            this.AddToolBtn.Location = new System.Drawing.Point(882, 390);
+            this.AddToolBtn.Name = "AddToolBtn";
+            this.AddToolBtn.Size = new System.Drawing.Size(230, 44);
+            this.AddToolBtn.TabIndex = 12;
+            this.AddToolBtn.Text = "Добавить";
+            this.AddToolBtn.UseVisualStyleBackColor = true;
+            this.AddToolBtn.Click += new System.EventHandler(this.AddToolBtn_Click);
             // 
-            // pictureBox1
+            // label7
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(371, 45);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(395, 372);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(187, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "введите цену(руб.)";
             // 
-            // label1
+            // PriceTB
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(523, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "картинка";
+            this.PriceTB.Location = new System.Drawing.Point(8, 225);
+            this.PriceTB.Name = "PriceTB";
+            this.PriceTB.Size = new System.Drawing.Size(299, 30);
+            this.PriceTB.TabIndex = 10;
             // 
-            // comboBox1
+            // label6
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(13, 149);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(299, 33);
-            this.comboBox1.TabIndex = 2;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(949, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "описание";
             // 
-            // label2
+            // LoadPicBtn
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 121);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(162, 25);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "выберите товар";
+            this.LoadPicBtn.BackColor = System.Drawing.Color.Snow;
+            this.LoadPicBtn.Location = new System.Drawing.Point(434, 390);
+            this.LoadPicBtn.Name = "LoadPicBtn";
+            this.LoadPicBtn.Size = new System.Drawing.Size(249, 44);
+            this.LoadPicBtn.TabIndex = 8;
+            this.LoadPicBtn.Text = "Загрузить картинку";
+            this.LoadPicBtn.UseVisualStyleBackColor = false;
+            this.LoadPicBtn.Click += new System.EventHandler(this.LoadPicBtn_Click);
             // 
-            // textBox1
+            // NameTB
             // 
-            this.textBox1.Location = new System.Drawing.Point(814, 45);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(355, 372);
-            this.textBox1.TabIndex = 4;
+            this.NameTB.Location = new System.Drawing.Point(9, 151);
+            this.NameTB.Name = "NameTB";
+            this.NameTB.Size = new System.Drawing.Size(298, 30);
+            this.NameTB.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "введите название:";
             // 
             // label3
             // 
@@ -133,41 +146,61 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "ДОБАВЛЕНИЕ ЗАПЧАСТИ:";
             // 
-            // label4
+            // OpisanieTB
             // 
-            this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(4, 4);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(238, 25);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "УДАЛЕНИЕ ЗАПЧАСТИ:";
+            this.OpisanieTB.Location = new System.Drawing.Point(814, 45);
+            this.OpisanieTB.Multiline = true;
+            this.OpisanieTB.Name = "OpisanieTB";
+            this.OpisanieTB.Size = new System.Drawing.Size(355, 327);
+            this.OpisanieTB.TabIndex = 4;
             // 
-            // label5
+            // label2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 45);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(185, 25);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "введите название:";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "выберите товар";
             // 
-            // textBox2
+            // TankComboBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 74);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(298, 30);
-            this.textBox2.TabIndex = 7;
+            this.TankComboBox.FormattingEnabled = true;
+            this.TankComboBox.Location = new System.Drawing.Point(8, 73);
+            this.TankComboBox.Name = "TankComboBox";
+            this.TankComboBox.Size = new System.Drawing.Size(299, 33);
+            this.TankComboBox.TabIndex = 2;
             // 
-            // button1
+            // label1
             // 
-            this.button1.BackColor = System.Drawing.Color.Snow;
-            this.button1.Location = new System.Drawing.Point(45, 373);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(249, 44);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Загрузить картинку";
-            this.button1.UseVisualStyleBackColor = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(500, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "картинка";
+            // 
+            // PicBox
+            // 
+            this.PicBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.PicBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PicBox.Location = new System.Drawing.Point(371, 45);
+            this.PicBox.Name = "PicBox";
+            this.PicBox.Size = new System.Drawing.Size(362, 327);
+            this.PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicBox.TabIndex = 0;
+            this.PicBox.TabStop = false;
+            // 
+            // ButtonPanel
+            // 
+            this.ButtonPanel.BackColor = System.Drawing.Color.Lime;
+            this.ButtonPanel.Controls.Add(this.button2);
+            this.ButtonPanel.Controls.Add(this.label4);
+            this.ButtonPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ButtonPanel.Location = new System.Drawing.Point(0, 440);
+            this.ButtonPanel.Name = "ButtonPanel";
+            this.ButtonPanel.Size = new System.Drawing.Size(1181, 137);
+            this.ButtonPanel.TabIndex = 1;
             // 
             // button2
             // 
@@ -179,14 +212,19 @@
             this.button2.Text = "Удалить запчасть";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // label6
+            // label4
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(949, 9);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 25);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "описание";
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(4, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(238, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "УДАЛЕНИЕ ЗАПЧАСТИ:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // AddToolForm
             // 
@@ -201,9 +239,9 @@
             this.Text = "Добавить/Удалить запчасть";
             this.InfoPanel.ResumeLayout(false);
             this.InfoPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
             this.ButtonPanel.ResumeLayout(false);
             this.ButtonPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -212,17 +250,21 @@
 
         private System.Windows.Forms.Panel InfoPanel;
         private System.Windows.Forms.Panel ButtonPanel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button LoadPicBtn;
+        private System.Windows.Forms.TextBox NameTB;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox OpisanieTB;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox TankComboBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox PicBox;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox PriceTB;
+        private System.Windows.Forms.Button AddToolBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
--- ShopOfTanks/AddToolForm.cs
+++ ShopOfTanks/AddToolForm.cs
@@ -15,6 +15,50 @@ namespace ShopOfTanks
         public AddToolForm()
         {
             InitializeComponent();
+
+            List<string> tanks_list = SQL_class.mySelect("SELECT id, name From tanks");
+            for (int i=0; i<tanks_list.Count; i+=2)
+            {
+                TankComboBox.Items.Add(tanks_list[i] + ". " + tanks_list[i+1]);
+            }
+        }
+
+        string FileName = "";
+        private void LoadPicBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)                                                                  //когда мы нажали ОК в выборе в проводнике
+            {
+                FileName = openFileDialog1.FileName;
+                PicBox.Load(FileName);
+                FileName = System.IO.Path.GetFileName(openFileDialog1.FileName);
+            }
+        }
+
+        private void AddToolBtn_Click(object sender, EventArgs e)
+        {
+            int a;
+            if (!Int32.TryParse(PriceTB.Text, out a))
+            {
+                MessageBox.Show("Цену указывать ТОЛЬКО в ЦИФРАХ!");
+                return;
+            }
+
+            if (NameTB.Text == "" || TankComboBox.Text == "" || PriceTB.Text == "")
+            {
+                MessageBox.Show("Все поля обязательны к заполнению!");
+                return;
+            }
+
+            string tank_id = TankComboBox.Text.Split(new string[] { ". " }, StringSplitOptions.None)[0];
+
+            //запись параметров объекта в SQL таблицу
+            SQL_class.myUpdate("INSERT INTO parts (name, price, tank_id, adres, opis) VALUES ('" + NameTB.Text + "', '" + PriceTB.Text + "', '" + tank_id + "', '" + FileName + "', '" + OpisanieTB.Text + "')");
+
+            if (NameTB.Text != "" && TankComboBox.Text != "" && PriceTB.Text != "")
+            {
+                MessageBox.Show("Запчасть успешно добавлена!");
+                Close();
+            }
         }
     }
 }
