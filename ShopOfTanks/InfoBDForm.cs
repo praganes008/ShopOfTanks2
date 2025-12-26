10f2056470c922b40dad7612b6274b8b984c986d
praganes008 <akim.ivan777@gmail.com>
2025-12-26 19:28:50 +0400

    0...

--- ShopOfTanks/InfoBDForm.cs
+++ ShopOfTanks/InfoBDForm.cs
@@ -23,75 +23,75 @@ namespace ShopOfTanks
             if (InfoCB.Text == "Танки")
             {
                 List<string> list = SQL_class.mySelect("SELECT id, name FROM tanks");
-                int y = 10;
+                int y = 15;
                 for (int i = 0; i < list.Count; i += 2)
                 {
                     Label lbl_id = new Label();
                     lbl_id.Location = new Point(20, y);
-                    lbl_id.Size = new Size(30, 15);
+                    lbl_id.Size = new Size(30, 20);
                     lbl_id.Text = list[i];
                     InfoPanel.Controls.Add(lbl_id);
 
                     Label lbl_name = new Label();
                     lbl_name.Location = new Point(55, y);
-                    lbl_name.Size = new Size(200, 15);
+                    lbl_name.Size = new Size(200, 20);
                     lbl_name.Text = list[i + 1];
                     InfoPanel.Controls.Add(lbl_name);
 
-                    y += 20;
+                    y += 25;
                 }
 
             }
 
             else if (InfoCB.Text == "Комплектующие")
             {
-                List<string> list = SQL_class.mySelect("SELECT id, name, tank_id FROM parts");
-                int y = 10;
-                for (int i = 0; i < list.Count; i += 2)
+                List<string> list1 = SQL_class.mySelect("SELECT id, name, tank_id FROM parts");
+                int y = 15;
+                for (int i = 0; i < list1.Count; i += 3)
                 {
                     Label lbl_id = new Label();
                     lbl_id.Location = new Point(20, y);
-                    lbl_id.Size = new Size(30, 15);
-                    lbl_id.Text = list[i];
+                    lbl_id.Size = new Size(30, 20);
+                    lbl_id.Text = list1[i];
                     InfoPanel.Controls.Add(lbl_id);
 
                     Label lbl_name = new Label();
                     lbl_name.Location = new Point(55, y);
-                    lbl_name.Size = new Size(200, 15);
-                    lbl_name.Text = list[i + 1];
+                    lbl_name.Size = new Size(250, 20);
+                    lbl_name.Text = list1[i + 1];
                     InfoPanel.Controls.Add(lbl_name);
 
-                    string car = SQL_class.mySelect("SELECT name FROM tanks WHERE id = '" + list[2] +"'")[0];
+                    string tank = SQL_class.mySelect("SELECT name FROM tanks WHERE id = '" + list1[2] + "' ")[0];
 
                     Label lbl_tank = new Label();
-                    lbl_tank.Location = new Point(260, y);
-                    lbl_tank.Size = new Size(200, 15);
-                    lbl_tank.Text = ;
+                    lbl_tank.Location = new Point(300, y);
+                    lbl_tank.Size = new Size(250, 20);
+                    lbl_tank.Text = tank;
                     InfoPanel.Controls.Add(lbl_tank);
 
-                    y += 20;
+                    y += 30;
                 }
             }
 
             else if (InfoCB.Text == "Пользователи")
             {
-                List<string> list = SQL_class.mySelect("SELECT id, name FROM users");
-                int y = 10;
-                for (int i = 0; i < list.Count; i += 2)
+                List<string> list2 = SQL_class.mySelect("SELECT id, name FROM users");
+                int y = 1;
+                for (int i = 0; i < list2.Count; i += 2)
                 {
                     Label lbl_id = new Label();
                     lbl_id.Location = new Point(20, y);
-                    lbl_id.Size = new Size(30, 15);
-                    lbl_id.Text = list[i];
+                    lbl_id.Size = new Size(30, 20);
+                    lbl_id.Text = list2[i];
                     InfoPanel.Controls.Add(lbl_id);
 
                     Label lbl_name = new Label();
                     lbl_name.Location = new Point(55, y);
-                    lbl_name.Size = new Size(200, 15);
-                    lbl_name.Text = list[i + 1];
+                    lbl_name.Size = new Size(200, 20);
+                    lbl_name.Text = list2[i + 1];
                     InfoPanel.Controls.Add(lbl_name);
 
-                    y += 20;
+                    y += 30;
                 }
             }
         }
